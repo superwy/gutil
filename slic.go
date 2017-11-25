18881dcce23b09267579b3226b1dfccee8f4ffaf
wangyang <superyfwy@gmail.com>
2017-11-25 22:01:31 +0800

    add Slice2Interface

--- slic.go
+++ slic.go
@@ -1,1 +1,19 @@
 package gutil
+
+import (
+	"github.com/pkg/errors"
+	"reflect"
+)
+
+func Slice2Interface(arg interface{}) []interface{} {
+	slice := reflect.ValueOf(arg)
+	if slice.Kind() != reflect.Slice {
+		panic(errors.New("bad slice"))
+	}
+	count := slice.Len()
+	result := make([]interface{}, count)
+	for i := 0; i < count; i++ {
+		result[i] = slice.Index(i).Interface()
+	}
+	return result
+}
