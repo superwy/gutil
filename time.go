e7f9d2ecb4201a7ea8ffca73ee1b94498d49d269
wangyang <superyfwy@gmail.com>
2018-05-31 07:38:51 +0800

    add tick method

--- time.go
+++ time.go
@@ -19,3 +19,25 @@ func TickParse(layout, value string) (int64, error) {
 	}
 	return 0, errors.New("empty value")
 }
+
+func TickDayNow() (tick int64) {
+	layout := "2006-01-02"
+	strDate := time.Now().Format(layout)
+	tick, _ = TickParse(layout, strDate)
+	return
+}
+
+func TickDayLast() (tick int64) {
+	layout := "2006-01-02"
+	strDate := time.Now().Format(layout)
+	tm, _ := time.ParseInLocation(layout, strDate, time.Local)
+	tm = tm.Add(time.Duration(23) * time.Hour)
+	tm = tm.Add(time.Duration(59) * time.Minute)
+	tm = tm.Add(time.Duration(59) * time.Second)
+	tick = tm.UnixNano() / 1e6
+	return
+}
+
+func TickNow() int64 {
+	return time.Now().UnixNano() / 1e6
+}
