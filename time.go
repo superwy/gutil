8155a4a6e182aa1565c48bf0f6a9e608c621a201
wangyang <superyfwy@gmail.com>
2018-06-09 10:52:15 +0800

    minor

--- time.go
+++ time.go
@@ -56,6 +56,6 @@ func YearMonth2Time(monthVal int, day int) time.Time {
 }
 
 func MonthDays(tm time.Time) int {
-	newMonth := time.Date(tm.Year(), tm.Month(), 0, 0, 0, 0, 0, tm.Location()).AddDate(0, 1, 0)
+	newMonth := time.Date(tm.Year(), tm.Month(), 1, 0, 0, 0, 0, tm.Location()).AddDate(0, 1, 0)
 	return newMonth.AddDate(0, 0, -1).Day()
 }
