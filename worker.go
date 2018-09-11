b3a7b922a41bc26007e8e0a93601b6586b400011
wangyang <superyfwy@gmail.com>
2018-09-11 12:57:01 +0800

    完善RunAsyncWorker

--- worker.go
+++ worker.go
@@ -1,6 +1,9 @@
 package gutil
 
-import "sync"
+import (
+	"errors"
+	"sync"
+)
 
 func RunAsyncWorker(fnWorkers ...func() error) (err error) {
 	if len(fnWorkers) <= 0 {
@@ -11,6 +14,18 @@ func RunAsyncWorker(fnWorkers ...func() error) (err error) {
 	wg.Add(len(fnWorkers))
 	for _, fnItemWork := range fnWorkers {
 		go func(itemWork func() error) {
+			defer func() {
+				if r := recover(); r != nil {
+					switch x := r.(type) {
+					case string:
+						err = errors.New(x)
+					case error:
+						err = x
+					default:
+						err = errors.New("Unknown panic")
+					}
+				}
+			}()
 			defer wg.Done()
 			if err != nil {
 				return
